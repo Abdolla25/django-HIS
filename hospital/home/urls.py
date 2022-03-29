1002ae6c0beaee51b949b33e8073b26a21116b4d
Abdolla25 <abdollaz225@gmail.com>
2022-03-29 10:22:29 +0200

    fix admin url

--- hospital/home/urls.py
+++ hospital/home/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib import admin
 
 from . import views
 
@@ -7,5 +8,6 @@ app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('contact/', views.contact, name='contact'),
-    path('p/<slug:slug>/', views.PageView.as_view(), name='page_details'),
+    re_path(r'^admin/', admin.site.urls, name='admin'),
+    path('<slug:slug>/', views.PageView.as_view(), name='page_details'),
 ]
